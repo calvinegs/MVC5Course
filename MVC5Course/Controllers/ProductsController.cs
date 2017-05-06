20706ff24fdab5e9b508565b427e0300402312c4
Calvin.egs <Calvin.egs@gmail.com>
2017-05-06 17:37:27 +0800

    1st Day end

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
+using MVC5Course.Models.ViewModels;
 
 namespace MVC5Course.Controllers
 {
@@ -15,11 +16,14 @@ namespace MVC5Course.Controllers
         private FabricsEntities db = new FabricsEntities();
 
         // GET: Products
-        public ActionResult Index()
+        public ActionResult Index(bool Active = false)
         {
             //return View(db.Product.ToList());
             //return View(db.Product.OrderByDescending(p => p.ProductName).Take(20));
-            return View(db.Product.Where(p => p.ProductName.StartsWith("w")).OrderByDescending(p=>p.ProductName));
+            return View(db.Product
+                .Where(p => p.Active.HasValue && p.Active.Value == Active)
+                .Where(p => p.ProductName.StartsWith("w"))
+                .OrderByDescending(p=>p.ProductName));
         }
 
         // GET: Products/Details/5
@@ -124,6 +128,21 @@ namespace MVC5Course.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public ActionResult ListProducts()
+        {
+            var data = db.Product
+                    .Where(p => p.Active == true)
+                    .Select(p => new ProductLiteVM ()
+                    {
+                        ProductId = p.ProductId,
+                        ProductName = p.ProductName,
+                        Price = p.Price,
+                        Stock = p.Stock
+                    })
+                    .Take(10);
+            return View(data);
         }
     }
 }
--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -2,8 +2,9 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(ProductMetaData))]
     public partial class Product
     {
@@ -13,19 +14,22 @@ namespace MVC5Course.Models
     {
         [Required]
         public int ProductId { get; set; }
-
+        [DisplayName("商品名稱")]
         [Required(ErrorMessage = "請輸入商品名稱")]
         [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
         [MinLength(3), MaxLength(30)]
         //[RegularExpression("(.+)-(.+)", ErrorMessage = "商品名稱格式錯誤!")]
         public string ProductName { get; set; }
+        [DisplayName("商品價格")]
         [Required(ErrorMessage = "請輸入商品價格")]
         [Range(0, 9999, ErrorMessage = "請輸入正確的商品價格")]
         [DisplayFormat(DataFormatString = "{0:0}",ApplyFormatInEditMode =true)]
         public Nullable<decimal> Price { get; set; }
+        [DisplayName("是否上架")]
         public Nullable<bool> Active { get; set; }
         [Required]
         [Range(0, 100, ErrorMessage = "請輸入正確的數量")]
+        [DisplayName("商品數量")]
         public Nullable<decimal> Stock { get; set; }
     
         public virtual ICollection<OrderLine> OrderLine { get; set; }
