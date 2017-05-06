20d7ba536df206bf0c09a934b363fc3acfd7fbaa
Calvin.egs <Calvin.egs@gmail.com>
2017-05-06 14:23:18 +0800

    DisplayFormat test

---
+++ MVC5Course/Models/Product.Partial.cs
@@ -0,0 +1,33 @@
+namespace MVC5Course.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+    }
+    
+    public partial class ProductMetaData
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "請輸入商品名稱")]
+        [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        [MinLength(3), MaxLength(30)]
+        //[RegularExpression("(.+)-(.+)", ErrorMessage = "商品名稱格式錯誤!")]
+        public string ProductName { get; set; }
+        [Required(ErrorMessage = "請輸入商品價格")]
+        [Range(0, 9999, ErrorMessage = "請輸入正確的商品價格")]
+        [DisplayFormat(DataFormatString = "{0:0}",ApplyFormatInEditMode =true)]
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<bool> Active { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "請輸入正確的數量")]
+        public Nullable<decimal> Stock { get; set; }
+    
+        public virtual ICollection<OrderLine> OrderLine { get; set; }
+    }
+}
